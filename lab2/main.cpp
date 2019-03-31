e4b2bf951453d8fbccd6af7deea4cab7c28b729c
shunyawatanabe <s.watte3212@gmail.com>
2019-03-31 16:48:05 +0545

    writing redirection works

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -70,12 +70,25 @@ void perform_pipe(vector<string>commands, list<string>& history, map<string, str
 			perform_action(words.at(0), history, words, paths);
 			exit(0);
 		}
-		// parent. wait for child process to die
+		// parent. close pipe and wait for child process to die
 		if (i != 0){
 			close(pipefd[in-2]);
 			close(pipefd[out-2]);
 		}
 		waitpid(pid, &status, 0);
+	}
+}
+
+void perform(string input, list<string>history, map<string, string>& paths){
+	// 1: basic operation
+	if (input.find("|") == -1) {
+		vector<string> words = split(input, " ");
+		perform_action(words.at(0), history, words, paths);
+	}
+	// 2: pipe operation 
+	else {
+		vector<string> commands = split(input, "|");
+		perform_pipe(commands, history, paths);
 	}
 }
 
@@ -95,17 +108,78 @@ int main(){
 		getline(cin, input);
 		execute_history(history, &input);
 		append_history(history, input);
-		
-		vector<string> commands = split(input, "|");
 
-		// basic operation
-		if (commands.size() == 1){
-			vector<string> words = split(commands.at(0), " ");	
-				perform_action(words.at(0), history, words, paths);
-			continue;
+
+		// output redirection 
+		if (input.find(">") != -1){
+			cout<<"detedcted >"<<endl;
+			vector<string> IOs = split(input, ">");
+			if (IOs.size() != 2){
+				cerr<<"wrong format for operator >."<<endl;
+				continue;
+			}
+			string input = IOs.at(0);
+			string filename = IOs.at(1);
+			filename.erase(remove(filename.begin(), filename.end(), ' '), filename.end());
+			cout <<"input: "<< input << endl;
+			cout <<"filename: "<< filename << endl;
+			int pipefd[2];
+			if (pipe(pipefd) == -1){
+				cerr<<"error piping"<<endl;
+				exit(0);
+			}
+			int pid = fork();
+			if (pid < 0){
+				cerr<<"error forking"<<endl;
+			}
+			if (pid == 0){
+				// child process. perform and write to pipe
+				close(pipefd[0]);
+				dup2(pipefd[1], STDOUT_FILENO);
+				close(pipefd[1]);
+
+				perform(input, history, paths);
+				exit(0);
+			}
+			else {
+				int pid = fork();
+				if (pid < 0){
+					cerr<<"error forking"<<endl;
+				}
+				if (pid == 0){
+					// child process two. receive output
+					close(pipefd[1]);
+					dup2(pipefd[0], STDIN_FILENO);
+					close(pipefd[0]);
+					wait(NULL);
+
+					// write to file
+					char data[255];
+					memset(data, 0, 255);
+					ofstream file;
+					file.open(filename);
+					read(STDIN_FILENO, data, 255);
+					file << data;
+					file.close();
+					exit(0);
+				}
+				// parent process
+				close(pipefd[0]);
+				close(pipefd[1]);
+				wait(NULL);
+				wait(NULL);
+			}
 		}
-		// pipe if there is |
-		perform_pipe(commands, history, paths);
+		// input redirection
+		else if (input.find("<") != -1){
+			cout<<"detedcted <"<<endl;
+
+		}
+		// no IO redirection
+		else {
+			cout<<"nothing detedcted"<<endl;
+			perform(input, history, paths);	
+		}
 	}
 	return 0;
 }
